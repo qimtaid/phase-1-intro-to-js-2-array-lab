cc4484e7425fe30a92540982e0e6aeb85bc78d32
Dan Koskei <qimtaid@gmail.com>
2024-03-22 23:56:41 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution here!
+
+// index.js
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+module.exports = {
+    cats,
+    destructivelyAppendCat
+};
